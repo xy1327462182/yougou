4806e8fb7ad260fa6e6ec4d48cf762047100e8f0
yang <1327462182@qq.com>
2020-08-28 21:04:39 +0800

    complete goods_detail

--- miniprogram/pages/cart/index.js
+++ miniprogram/pages/cart/index.js
@@ -8,6 +8,51 @@ Page({
 
   },
 
+  /*添加收货地址按钮
+  ps: 调用wx.chooseAddress()添加收货地址
+      调用wx.getSetting()获取用户授权状态
+
+  1.用户点击确定获取收货地址 
+    authSetting scope.address值为true
+
+  2.用户点击取消获取收货地址
+    authSetting scope.address值为false
+
+  3.用户未点击按钮
+    authSetting scope.address值为undefined
+  */
+  handelAddressAdd(e){
+    // wx.chooseAddress({
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+
+    wx.getSetting({
+      success(res1){
+        if (res1.authSetting['scope.address'] || res1.authSetting['scope.address'] == undefined) {
+          //直接调用wx.chooseAddress()
+          wx.chooseAddress({
+            success: (res2) => {
+              console.log(res2)
+            },
+          })
+        } else {
+          //调用wx.openSetting() 引导用户授权
+          wx.openSetting({
+            success(res3){
+              if (res3.authSetting['scope.address']) {
+                //如果用户更改授权状态为true再调用wx.chooseAddress()
+                wx.chooseAddress()
+              }
+            }
+          })
+        }
+        
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
--- miniprogram/pages/goods_detail/index.js
+++ miniprogram/pages/goods_detail/index.js
@@ -1,19 +1,88 @@
 // pages/goods_detail/index.js
+
+//引入请求的promise
+import { request } from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //商品数据
+    goodsDetails: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let {goods_id}=options
+    this.getGoodsDetails(goods_id)
   },
+
+  //获取商品数据
+  async getGoodsDetails(goods_id){
+    let res= await request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/goods/detail",
+      data: {goods_id}
+    })
+    this.setData({
+      goodsDetails: {
+        goods_id: res.data.message.goods_id,
+        goods_name: res.data.message.goods_name,
+        goods_price: res.data.message.goods_price,
+        goods_introduce: res.data.message.goods_introduce.replace(/\.webp/g, ".jpg"),
+        pics: res.data.message.pics
+      }
+    })
+  },
+
+  // 轮播图点击预览大图
+  handelPicTap(e){
+    //获取原来的数据
+    let pics=this.data.goodsDetails.pics
+    //图片路径数组遍历 获取pics_mid数组
+    let urls=pics.map(n=>n.pics_mid)
+    //获取当前点击的图片src
+    let current=e.currentTarget.dataset.src
+    wx.previewImage({
+      urls,
+      current,
+    })
+  },
+
+  //加入购物车
+  handelAddCart(){
+    //获取缓存中的购物车数据
+    //如果本来没有则获取空的数组
+    let cart=wx.getStorageSync('cart')||[]
+
+    //调用数组的findIndex方法 判断购物车数组中是否有本商品
+    let index=cart.findIndex(v=>v.goods_id===this.data.goodsDetails.goods_id)
+
+    if (index==-1) {
+      //数组没有本商品
+      //商品数量设置为1
+      this.data.goodsDetails.num = 1
+      //添加到购物车数组中
+      cart.push(this.data.goodsDetails)
+    } else {
+      //数组本就有本商品
+      //拿到数组本商品数据 num++
+      cart[index].num++
+    }
+
+    //最后将新数据存入缓存中
+    wx.setStorageSync('cart', cart)
+    //弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: "success",
+      mask: true
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
